b6d36a34d920349a7da6db6ef1d08f5f906998d3
Claudio Perroni <c.perroni@ifortech.com>
2024-04-29 12:02:36 +0200

    cron 5 min

--- support-api/app/Console/Kernel.php
+++ support-api/app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel {
     protected function schedule(Schedule $schedule): void {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->job(new \App\Jobs\TicketStats)->hourly();
+        $schedule->job(new \App\Jobs\TicketStats)->everyFiveMinutes(); //ogni 5 min
         // $schedule->job(new \App\Jobs\TicketStats)->everyMinute();
     }
 
