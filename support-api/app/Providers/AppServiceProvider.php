aca23348dacc2e6cf79b77e54cb0254ace2732d1
erasmosalsanoifortech <e.salsano@ifortech.com>
2024-06-04 16:49:24 +0200

    log-viewer

--- support-api/app/Providers/AppServiceProvider.php
+++ support-api/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        LogViewer::auth(function ($request) {  
+            $user = $request->user();
+            return $user && $user->is_admin;   
+            // return $request->user()            
+            // && in_array($request->user()->email, [                
+            //     'john@example.com',            
+            // ]);    
+        });
     }
 }
