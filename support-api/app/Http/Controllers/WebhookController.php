834f139dab14926ac1297699fa89ddc79fe08b85
Claudio Perroni <c.perroni@ifortech.com>
2024-07-03 08:46:34 +0200

    webhook errori tracciamento

--- support-api/app/Http/Controllers/WebhookController.php
+++ support-api/app/Http/Controllers/WebhookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\StoreDgveryLive;
+use App\Jobs\StoreDgveryTrackingError;
 use Illuminate\Http\Request;
 
 class WebhookController extends Controller {
@@ -12,9 +13,12 @@ class WebhookController extends Controller {
         switch ($request->header('X-Support-Webhook-Event')) {
             case 'ticket.new_live_academelearning':
                 // Handle ticket created event
-
-
                 dispatch(new StoreDgveryLive($request->all()));
+
+                break;
+            case 'ticket.new_tracking_error_academelearning':
+                // Handle ticket created event
+                dispatch(new StoreDgveryTrackingError($request->all()));
 
                 break;
             default:
