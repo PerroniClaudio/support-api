74d4845c102f260f8c23a73a573eeb73573d78cc
erasmosalsanoifortech <e.salsano@ifortech.com>
2024-07-08 16:34:06 +0200

    Modifica generazione massiva ticket

--- support-api/app/Imports/TicketsImport.php
+++ support-api/app/Imports/TicketsImport.php
@@ -32,10 +32,6 @@ class TicketsImport implements ToCollection
     */
     public function collection(Collection $rows)
     {
-        // Gestire qui i dati aggiuntivi
-
-        // Questo valore dovrà essere passato dal frontend
-        // $mergeRows = isset($this->additionalData->formData->merge_rows) ? $this->additionalData->formData->merge_rows : false;
         $mergeRows = $this->additionalData['formData']->merge_rows;
 
         $user = $this->additionalData['user'];
@@ -57,53 +53,14 @@ class TicketsImport implements ToCollection
     
                 foreach ($distinctValues as $currentValue) {
     
-                    // Genera il ticket
-                    // {
-                    //     "company":"1",
-                    //     "isRequest":false,
-                    //     "isProblem":true,
-                    //     "category":10,
-                    //     "type_id":26,
-                    //     "description":"dqwd",
-                    //     "messageData":{
-                    //         "description":"dqwd",
-                    //         "office":"1",
-                    //         "referer_it":3
-                    //     }
-                    // }
-    
                     $formData->messageData->Identificativo = $currentValue;
     
                     $ticketType = TicketType::find($formData->type_id);
                     $group = $ticketType->groups->first();
                     $groupId = $group ? $group->id : null;
-
-                    $createFile = true;
-
-                    // Descrizione per questo ticket specifico (viene modificata se c'è solo una riga e non si deve creare il file)
-                    $rowDescription = $formData->description;
-
-                    // Filtra le righe da raggruppare
-                    $filteredRows = $rows->filter(function ($row) use ($currentValue) {
-                        return isset($row[0]) && $row[0] == $currentValue;
-                    });
-
-                    if ($filteredRows->count() == 1) {
-                        $createFile = false;
-                        $addToDescription = '';
-                        foreach ($filteredRows->first() as $n => $value) {
-                            $addToDescription .= "\n" . $rows[0][$n] . ": " . $value;
-                        }
-
-                        $rowDescription .= $addToDescription;
-                    }
-
-                    // messageData per questo ticket specifico (viene modificato se c'è solo una riga e non si deve creare il file)
-                    $rowMessageData = clone $formData->messageData;
-                    $rowMessageData->description = $rowDescription;
     
                     $ticket = Ticket::create([
-                        'description' => $rowDescription,
+                        'description' => $formData->description,
                         'type_id' => $ticketType->id,
                         'group_id' => $groupId,
                         'user_id' => $user->id,
@@ -126,69 +83,49 @@ class TicketsImport implements ToCollection
                     TicketMessage::create([
                         'ticket_id' => $ticket->id,
                         'user_id' => $user->id,
-                        'message' => json_encode($rowMessageData),
-                        // 'is_read' => 1
+                        'message' => json_encode($formData->messageData),
                     ]);
     
                     TicketMessage::create([
                         'ticket_id' => $ticket->id,
                         'user_id' => $user->id,
-                        'message' => $rowDescription,
-                        // 'is_read' => 0
+                        'message' => $formData->description,
                     ]);
     
                     $brand_url = $ticket->brandUrl();
                     dispatch(new SendOpenTicketEmail($ticket, $brand_url));
     
                     // Salva il file come allegato del ticket
-                    // $filteredRows = $rows->filter(function ($row) use ($currentValue) {
-                    //     return isset($row[0]) && $row[0] == $currentValue;
-                    // });
+                    $filteredRows = $rows->filter(function ($row) use ($currentValue) {
+                        return isset($row[0]) && $row[0] == $currentValue;
+                    });
     
                     $fileName = '';
     
                     // Crea il file se ci sono più righe raggruppate
-                    if($createFile){
-                        if ($filteredRows->isNotEmpty()) {
-                            $filteredRows->prepend($rows[0]); // Add the first row to the filtered rows
-        
-                            $fileName = 'file_' . $currentValue . '_' . time() . '.xlsx';
-                        }
-                        $path = "tickets/" . $ticket->id . "/";
+                    if ($filteredRows->isNotEmpty()) {
+                        $filteredRows->prepend($rows[0]); // Add the first row to the filtered rows
     
-                        $export = new RowsExport($filteredRows);
-                        Excel::store($export, $path . $fileName, "gcs");
-                        $extension = pathinfo($fileName, PATHINFO_EXTENSION);
-                        $size = Storage::disk('gcs')->size($path . $fileName);
-                        $ticketFile = TicketFile::create([
-                            'ticket_id' => $ticket->id,
-                            'filename' => $fileName,
-                            'path' => $path . $fileName,
-                            'extension' => $extension,
-                            'mime_type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                            'size' => $size,
-                        ]);
+                        $fileName = 'file_' . $currentValue . '_' . time() . substr(uniqid(), -3) . '.xlsx';
                     }
+                    $path = "tickets/" . $ticket->id . "/";
+
+                    $export = new RowsExport($filteredRows);
+                    Excel::store($export, $path . $fileName, "gcs");
+                    $extension = pathinfo($fileName, PATHINFO_EXTENSION);
+                    $size = Storage::disk('gcs')->size($path . $fileName);
+                    $ticketFile = TicketFile::create([
+                        'ticket_id' => $ticket->id,
+                        'filename' => $fileName,
+                        'path' => $path . $fileName,
+                        'extension' => $extension,
+                        'mime_type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                        'size' => $size,
+                    ]);
                 }
     
             } else {
                 foreach ($rows as $index => $row) {
-    
-                    // Genera il ticket
-                    // {
-                    //     "company":"1",
-                    //     "isRequest":false,
-                    //     "isProblem":true,
-                    //     "category":10,
-                    //     "type_id":26,
-                    //     "description":"dqwd",
-                    //     "messageData":{
-                    //         "description":"dqwd",
-                    //         "office":"1",
-                    //         "referer_it":3
-                    //     }
-                    // }
-
                     if ($index == 0) {
                         continue;
                     }
@@ -196,21 +133,13 @@ class TicketsImport implements ToCollection
                     $currentValue = $row[0];
     
                     $formData->messageData->Identificativo = $currentValue;
-    
-                    // Essendoci una riga sola in questo caso si aggiungono i dati nella descrizione e si evita di creare file inutilmente.
-                    $addToDescription = '';
-                    foreach ($row as $n => $value) {
-                        $addToDescription .= "\n" . $rows[0][$n] . ": " . $value;
-                    }
-    
-                    $rowDescription = $formData->description . $addToDescription;
 
                     $ticketType = TicketType::find($formData->type_id);
                     $group = $ticketType->groups->first();
                     $groupId = $group ? $group->id : null;
     
                     $ticket = Ticket::create([
-                        'description' => $rowDescription,
+                        'description' => $formData->description,
                         'type_id' => $ticketType->id,
                         'group_id' => $groupId,
                         'user_id' => $user->id,
@@ -229,51 +158,47 @@ class TicketsImport implements ToCollection
     
                     cache()->forget('user_' . $user->id . '_tickets');
                     cache()->forget('user_' . $user->id . '_tickets_with_closed');
-    
-                    $rowMessageData = clone $formData->messageData;
-                    $rowMessageData->description = $rowDescription;
 
                     TicketMessage::create([
                         'ticket_id' => $ticket->id,
                         'user_id' => $user->id,
-                        'message' => json_encode($rowMessageData),
-                        // 'is_read' => 1
+                        'message' => json_encode($formData->messageData),
                     ]);
     
                     TicketMessage::create([
                         'ticket_id' => $ticket->id,
                         'user_id' => $user->id,
-                        'message' => $rowDescription,
-                        // 'is_read' => 0
+                        'message' => $formData->description,
                     ]);
     
                     $brand_url = $ticket->brandUrl();
                     dispatch(new SendOpenTicketEmail($ticket, $brand_url));
     
                     // Salva il file come allegato del ticket
-                    // $filteredRows = $rows->filter(function ($row) use ($currentValue) {
-                    //     return isset($row[0]) && $row[0] == $currentValue;
-                    // });
+                    $filteredRows = collect([$row]);
+
+                    $fileName = '';
     
-                    // $fileName = '';
+                    if ($filteredRows->isNotEmpty()) {
+                        $filteredRows->prepend($rows[0]); // Add the first row to the filtered rows
     
-                    // if ($filteredRows->isNotEmpty()) {
-                    //     $filteredRows->prepend($rows[0]); // Add the first row to the filtered rows
-    
-                    //     $fileName = 'file_' . $currentValue . '_' . time() . '.xlsx';
-                    // }
-                    // $path = "tickets/" . $ticket->id . "/";
-                    // Excel::store(new FromCollection($filteredRows), $path, $fileName, "gcs");
-                    // $extension = pathinfo($fileName, PATHINFO_EXTENSION);
-                    // $size = Storage::disk('gcs')->size($path . $fileName);
-                    // $ticketFile = TicketFile::create([
-                    //     'ticket_id' => $ticket->id,
-                    //     'filename' => $fileName,
-                    //     'path' => $path,
-                    //     'extension' => $extension,
-                    //     'mime_type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                    //     'size' => $size,
-                    // ]);
+                        $fileName = 'file_' . $currentValue . '_' . time() . substr(uniqid(), -3) . '.xlsx';
+                    }
+                    $path = "tickets/" . $ticket->id . "/";
+                    
+                    $export = new RowsExport($filteredRows);
+                    Excel::store($export, $path . $fileName, "gcs");
+                    $extension = pathinfo($fileName, PATHINFO_EXTENSION);
+                    $size = Storage::disk('gcs')->size($path . $fileName);
+                    $ticketFile = TicketFile::create([
+                        'ticket_id' => $ticket->id,
+                        'filename' => $fileName,
+                        'path' => $path . $fileName,
+                        'extension' => $extension,
+                        'mime_type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                        'size' => $size,
+                    ]);
+                    
                 }
             }
         } catch (\Exception $e) {
