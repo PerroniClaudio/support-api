2b13d628fcff52d5eab307d2f57f9edcb5db65d8
erasmosalsanoifortech <e.salsano@ifortech.com>
2024-07-17 16:43:57 +0200

    modifica throttle

--- support-api/app/Http/Kernel.php
+++ support-api/app/Http/Kernel.php
@@ -54,7 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'api' => [
-            'throttle:1200,1',
+            // 'throttle:1200,1',
         ],
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
