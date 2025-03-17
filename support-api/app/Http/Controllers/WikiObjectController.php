550c81ca2f5b56a68df84238262f6eae675376d5
Claudio Perroni <c.perroni@ifortech.com>
2025-03-17 19:18:42 +0100

    ripristino vecchie funzioni

--- support-api/app/Http/Controllers/WikiObjectController.php
+++ support-api/app/Http/Controllers/WikiObjectController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\WikiObject;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 
 class WikiObjectController extends Controller {
     /**
@@ -21,19 +22,172 @@ class WikiObjectController extends Controller {
             ], 401);
         }
 
-        $folder = base64_decode($request->folder);
-        $wikiObjects = WikiObject::where('path', $folder)
-            ->with('user')
-            ->orderByRaw("type = 'folder' DESC")
-            ->orderBy('created_at', 'asc')
-            ->get();
+        if (isset($request->filter) && ($request->filter != 'all')) {
 
+            $filters = json_decode($request->filter, true);
+
+            $mimeTypeIn = [];
+            $createdAtFilter = [];
+
+            foreach ($filters as $key => $value) {
+                switch ($key) {
+                    case 'pdf':
+                        if ($value) {
+                            $mimeTypeIn[] = "application/pdf";
+                        }
+                        break;
+                    case 'word':
+                        if ($value) {
+                            $mimeTypeIn[] = "application/msword";
+                            $mimeTypeIn[] = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+                        }
+                        break;
+                    case 'excel':
+                        if ($value) {
+                            $mimeTypeIn[] = "application/vnd.ms-excel";
+                            $mimeTypeIn[] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                        }
+                        break;
+                    case 'powerpoint':
+                        if ($value) {
+                            $mimeTypeIn[] = "application/vnd.ms-powerpoint";
+                            $mimeTypeIn[] = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+                        }
+                        break;
+                    case 'archive':
+                        if ($value) {
+                            $mimeTypeIn[] = "application/zip";
+                        }
+                        break;
+                    case 'dateFrom':
+                        if ($value) {
+                            $createdAtFilter[] = ['created_at', '>=', $value];
+                        }
+                        break;
+                    case 'dateTo':
+                        if ($value) {
+                            $createdAtFilter[] = ['created_at', '<=', $value];
+                        }
+                        break;
+                }
+            }
+
+            if (count($mimeTypeIn) == 0) {
+                $mimeTypeIn = ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/zip'];
+            }
+
+            if (count($createdAtFilter) == 0) {
+                $createdAtFilter = [['created_at', '>=', '2000-01-01']];
+            }
+
+            $wikiObjects = WikiObject::where('type', 'file')
+                ->whereIn('mime_type', $mimeTypeIn)
+                ->where($createdAtFilter)
+                ->with('user')
+                ->orderBy('created_at', 'asc')
+                ->get();
+        } else {
+
+            $folder = base64_decode($request->folder);
+            $wikiObjects = WikiObject::where('path', $folder)
+                ->with('user')
+                ->orderByRaw("type = 'folder' DESC")
+                ->orderBy('created_at', 'asc')
+                ->get();
+        }
 
 
         return response([
             'files' => $wikiObjects,
         ], 200);
     }
+
+    public function public(Request $request) {
+
+        $user = $request->user();
+
+        if (isset($request->filter) && ($request->filter != 'all')) {
+
+            $filters = json_decode($request->filter, true);
+
+            $mimeTypeIn = [];
+            $createdAtFilter = [];
+
+            foreach ($filters as $key => $value) {
+                switch ($key) {
+                    case 'pdf':
+                        if ($value) {
+                            $mimeTypeIn[] = "application/pdf";
+                        }
+                        break;
+                    case 'word':
+                        if ($value) {
+                            $mimeTypeIn[] = "application/msword";
+                            $mimeTypeIn[] = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+                        }
+                        break;
+                    case 'excel':
+                        if ($value) {
+                            $mimeTypeIn[] = "application/vnd.ms-excel";
+                            $mimeTypeIn[] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                        }
+                        break;
+                    case 'powerpoint':
+                        if ($value) {
+                            $mimeTypeIn[] = "application/vnd.ms-powerpoint";
+                            $mimeTypeIn[] = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+                        }
+                        break;
+                    case 'archive':
+                        if ($value) {
+                            $mimeTypeIn[] = "application/zip";
+                        }
+                        break;
+                    case 'dateFrom':
+                        if ($value) {
+                            $createdAtFilter[] = ['created_at', '>=', $value];
+                        }
+                        break;
+                    case 'dateTo':
+                        if ($value) {
+                            $createdAtFilter[] = ['created_at', '<=', $value];
+                        }
+                        break;
+                }
+            }
+
+            if (count($mimeTypeIn) == 0) {
+                $mimeTypeIn = ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/zip'];
+            }
+
+            if (count($createdAtFilter) == 0) {
+                $createdAtFilter = [['created_at', '>=', '2000-01-01']];
+            }
+
+            $wikiObjects = WikiObject::where('type', 'file')
+                ->whereIn('mime_type', $mimeTypeIn)
+                ->where($createdAtFilter)
+                ->where('is_public', 1)
+                ->with('user')
+                ->orderBy('created_at', 'asc')
+                ->get();
+        } else {
+
+            $folder = base64_decode($request->folder);
+            $wikiObjects = WikiObject::where('path', $folder)
+                ->where('is_public', 1)
+                ->with('user')
+                ->orderByRaw("type = 'folder' DESC")
+                ->orderBy('created_at', 'asc')
+                ->get();
+        }
+
+
+        return response([
+            'files' => $wikiObjects,
+        ], 200);
+    }
+
 
     /**
      * Show the form for creating a new resource.
@@ -66,50 +220,60 @@ class WikiObjectController extends Controller {
             'is_public' => 'required|boolean',
         ]);
 
-        if ($request->type == 'folder') {
+        $company_id = isset($request->company_id) ? $request->company_id : null;
 
 
-            $company_id = isset($request->company_id) ? $request->company_id : null;
+
+        if ($validated['type'] == 'folder') {
+            $name = $request->name;
+
             $wikiObject = WikiObject::create([
-                'name' => $validated['name'],
-                'uploaded_name' => $validated['name'],
+                'name' => $name,
+                'uploaded_name' => $name,
+                'type' => $validated['type'],
                 'mime_type' => 'folder',
-                'type' => $validated['type'],
                 'path' => $validated['path'],
                 'is_public' => $validated['is_public'],
-                'company_id' => $company_id,
+                'company_id' => null,
                 'uploaded_by' => $user->id,
+                'file_size' => 0,
             ]);
 
             return response([
                 'wikiObject' => $wikiObject,
             ], 201);
-        } else  if ($request->file('file') != null) {
+        } else {
+            if ($request->file('file') != null) {
 
-            $file = $request->file('file');
+                $file = $request->file('file');
 
-            $uploaded_name = time() . '_' . $file->getClientOriginalName();
-            $mime_type = $file->getClientMimeType();
-            $file_size = $file->getSize();
-            $bucket_path = 'wiki_objects' . $validated['path'] . '';
-            $file->storeAs($bucket_path, $uploaded_name, 'gcs');
+                $uploaded_name = time() . '_' . $file->getClientOriginalName();
+                $mime_type = $file->getClientMimeType();
+                $file_size = $file->getSize();
+                $bucket_path = 'wiki_objects' . $validated['path'] . '';
+                $file->storeAs($bucket_path, $uploaded_name, 'gcs');
 
-            $company_id = isset($request->company_id) ? $request->company_id : null;
-            $wikiObject = WikiObject::create([
-                'name' => $validated['name'],
-                'uploaded_name' => $uploaded_name,
-                'type' => $validated['type'],
-                'mime_type' => $mime_type,
-                'path' => $validated['path'],
-                'is_public' => $validated['is_public'],
-                'company_id' => $company_id,
-                'uploaded_by' => $user->id,
-                'file_size' => $file_size,
-            ]);
+                $company_id = isset($request->company_id) ? $request->company_id : null;
+                $wikiObject = WikiObject::create([
+                    'name' => $validated['name'],
+                    'uploaded_name' => $uploaded_name,
+                    'type' => $validated['type'],
+                    'mime_type' => $mime_type,
+                    'path' => $validated['path'],
+                    'is_public' => $validated['is_public'],
+                    'company_id' => $company_id,
+                    'uploaded_by' => $user->id,
+                    'file_size' => $file_size,
+                ]);
 
-            return response([
-                'wikiObject' => $wikiObject,
-            ], 201);
+                return response([
+                    'wikiObject' => $wikiObject,
+                ], 201);
+            } else {
+                return response([
+                    'message' => 'File is required.',
+                ], 400);
+            }
         }
     }
 
@@ -146,6 +310,47 @@ class WikiObjectController extends Controller {
 
         return response([
             'message' => 'WikiObject soft deleted successfully.',
+        ], 200);
+    }
+
+    public function searchPublic(Request $request) {
+        $user = $request->user();
+
+
+        $search = $request->search;
+
+        $wikiObjects = WikiObject::query()->when($search, function (Builder $q, $value) {
+            /** 
+             * @disregard Intelephense non rileva il metodo whereIn
+             */
+            return $q->whereIn('id', WikiObject::search($value)->keys());
+        })->where('is_public', 1)->with('user')->get();
+
+        return response([
+            'files' => $wikiObjects,
+        ], 200);
+    }
+
+    public function search(Request $request) {
+        $user = $request->user();
+
+        if ($user["is_admin"] != 1) {
+            return response([
+                'message' => 'The user must be an admin.',
+            ], 401);
+        }
+
+        $search = $request->search;
+
+        $wikiObjects = WikiObject::query()->when($search, function (Builder $q, $value) {
+            /** 
+             * @disregard Intelephense non rileva il metodo whereIn
+             */
+            return $q->whereIn('id', WikiObject::search($value)->keys());
+        })->with('user')->get();
+
+        return response([
+            'files' => $wikiObjects,
         ], 200);
     }
 }
