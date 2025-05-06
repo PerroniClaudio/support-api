2b1dd8a74586e9fd6f0d42294ef378ac23ca1faf
erasmosalsanoifortech <e.salsano@ifortech.com>
2025-05-06 16:43:53 +0200

    Rimodifica formattazione tempo esportazione excel

--- support-api/app/Exports/TicketsExport.php
+++ support-api/app/Exports/TicketsExport.php
@@ -125,7 +125,7 @@ class TicketsExport implements FromArray {
             $closingDate = $closingUpdate ? $closingUpdate->created_at : null;
 
             $expectedProcessingTime = $ticket->expected_processing_time 
-                ? str_pad(floor($ticket->expected_processing_time / 60), 2, '0', STR_PAD_LEFT) . ":" . str_pad($ticket->expected_processing_time % 60, 2, '0', STR_PAD_LEFT) . ":00"
+                ? floor($ticket->expected_processing_time / 60) . ":" . str_pad($ticket->expected_processing_time % 60, 2, '0', STR_PAD_LEFT) . ":00"
                 : "Non definito";
 
             // $processingTimeHours= $ticket->actual_processing_time ? floor($ticket->actual_processing_time / 60) : 0;
@@ -134,7 +134,7 @@ class TicketsExport implements FromArray {
             //     . ((!$processingTimeHours || !$processingTimeMinutes) ? "" : "e ")
             //     . (!!$processingTimeMinutes ? ($processingTimeMinutes . ($processingTimeMinutes > 1 ? " minuti" : " minuto")) : "");
             $processingTime = $ticket->actual_processing_time 
-                ? str_pad(floor($ticket->actual_processing_time / 60), 2, '0', STR_PAD_LEFT) . ":" . str_pad($ticket->actual_processing_time % 60, 2, '0', STR_PAD_LEFT) . ":00"
+                ? floor($ticket->actual_processing_time / 60) . ":" . str_pad($ticket->actual_processing_time % 60, 2, '0', STR_PAD_LEFT) . ":00"
                 : "Non definito";
 
             $waiting_times = $ticket->waitingTimes();
@@ -145,7 +145,7 @@ class TicketsExport implements FromArray {
             // $waitingTime = (!!$waitingTimeHours ? ($waitingTimeHours . ($waitingTimeHours > 1 ? " ore " : " ora ")) : "") 
             //     . ((!$waitingTimeHours || !$waitingTimeMinutes) ? "" : "e ")
             //     . (!!$waitingTimeMinutes ? ($waitingTimeMinutes . ($waitingTimeMinutes > 1 ? " minuti" : " minuto")) : "");
-            $waitingTime = str_pad(floor($waiting_hours), 2, '0', STR_PAD_LEFT) . ":" . str_pad(($waiting_hours - floor($waiting_hours)) * 60, 2, '0', STR_PAD_LEFT) . ":00";
+            $waitingTime = floor($waiting_hours) . ":" . str_pad(($waiting_hours - floor($waiting_hours)) * 60, 2, '0', STR_PAD_LEFT) . ":00";
 
             
             $workModes = config('app.work_modes');
