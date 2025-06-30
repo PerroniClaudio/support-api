aaa0e56fec33bed84f70646be2cbdbdc741d2c16
Claudio Perroni <c.perroni@ifortech.com>
2025-06-30 10:33:21 +0200

    fs per key google cloud

--- support-api/config/filesystems.php
+++ support-api/config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -59,7 +59,19 @@ return [
         'gcs' => [
             'driver' => 'gcs',
             'key_file_path' => env('GOOGLE_CLOUD_KEY_FILE', null), // optional: /path/to/service-account.json
-            'key_file' => [], // optional: Array of data that substitutes the .json file (see below)
+            'key_file' => [
+                'type' => env('GOOGLE_CLOUD_KEY_FILE_TYPE', 'service_account'), // optional: service_account|user_account
+                'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'), // optional: is included in key file
+                'private_key_id' => env('GOOGLE_CLOUD_PRIVATE_KEY_ID', 'your-private-key-id'), // optional: is included in key file
+                'private_key' => env('GOOGLE_CLOUD_PRIVATE_KEY', 'your-private-key'), // optional: is included in key file
+                'client_email' => env('GOOGLE_CLOUD_CLIENT_EMAIL', 'your-client-email'), // optional: is included in key file
+                'client_id' => env('GOOGLE_CLOUD_CLIENT_ID', 'your-client-id'), // optional: is included in key file
+                'auth_uri' => env('GOOGLE_CLOUD_AUTH_URI', 'https://accounts.google.com/o/oauth2/auth'), // optional: is included in key file
+                'token_uri' => env('GOOGLE_CLOUD_TOKEN_URI', 'https://oauth2.googleapis.com/token'),
+                'auth_provider_x509_cert_url' => env('GOOGLE_CLOUD_AUTH_PROVIDER_X509_CERT_URL', 'https://www.googleapis.com/oauth2/v1/certs'), // optional: is included in key file
+                'client_x509_cert_url' => env('GOOGLE_CLOUD_CLIENT_X509_CERT_URL', 'https://www.googleapis.com/robot/v1/metadata/x509/your-client-email'), // optional: is included in key file
+                'universe_domain' => env('GOOGLE_CLOUD_UNIVERSE_DOMAIN', 'googleapis.com'), // optional: is included in key file
+            ], // optional: Array of data that substitutes the .json file (see below)
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'), // optional: is included in key file
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
             'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', ''), // optional: /default/path/to/apply/in/bucket
@@ -67,7 +79,7 @@ return [
             'apiEndpoint' => env('GOOGLE_CLOUD_STORAGE_API_ENDPOINT', null), // set storageClient apiEndpoint
             'visibility' => 'noPredefinedVisibility', // optional: public|private
             'visibility_handler' => null, // optional: set to \League\Flysystem\GoogleCloudStorage\UniformBucketLevelAccessVisibility::class to enable uniform bucket level access
-            'metadata' => ['cacheControl'=> 'public,max-age=86400'], // optional: default metadata
+            'metadata' => ['cacheControl' => 'public,max-age=86400'], // optional: default metadata
         ],
 
     ],
